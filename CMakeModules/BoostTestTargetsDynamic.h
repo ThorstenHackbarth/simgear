ee230e49a281ab53812b431aca086c34e30b4133
Carlo Bramini <carlo_bramini@users.sourceforge.net>
2026-02-03 09:58:52 +0000

    BOOST: fix warning on BOOST_TEST_DYN_LINK

--- CMakeModules/BoostTestTargetsDynamic.h
+++ CMakeModules/BoostTestTargetsDynamic.h
@@ -1,8 +1,13 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-FileCopyrightText: 2014 Thomas Geymayer
+
 // Small header computed by CMake to set up boost test.
 // include AFTER #define BOOST_TEST_MODULE whatever
 // but before any other boost test includes.
 
 // Using the Boost UTF dynamic library
 
-#define BOOST_TEST_DYN_LINK
+#ifndef BOOST_TEST_DYN_LINK
+    #define BOOST_TEST_DYN_LINK
+#endif
 #include <boost/test/unit_test.hpp>
